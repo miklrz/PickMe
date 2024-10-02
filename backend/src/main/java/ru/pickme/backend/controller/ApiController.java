ea021db041f18a3a83512ce6ed1cbdf7ee9f1822
hxastur <arz.mikhail@gmail.com>
2024-10-02 12:12:21 +0300

    crud done

--- backend/src/main/java/ru/pickme/backend/controller/ApiController.java
+++ backend/src/main/java/ru/pickme/backend/controller/ApiController.java
@@ -1,22 +1,53 @@
 package ru.pickme.backend.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.pickme.backend.model.User;
 import ru.pickme.backend.service.UserService;
 
-import java.util.List;
-
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-     private final UserService userService;
+    private final UserService userService;
 
-    public ApiController(UserService userService){
+    public ApiController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/users")
-    public Iterable<User> getUsers(){
-        return userService.getUsers();
+    public ResponseEntity<Iterable<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User>getUser(@PathVariable("id") int id) {
+        return userService.getUser(id)
+                .map(user -> new ResponseEntity<>(user,HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User createdUser = userService.addUser(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdUser);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> editUser(@RequestBody User user, @PathVariable int id){
+        Optional<User> updatedUser = userService.editUser(user,id);
+        return updatedUser
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id){
+        boolean deleted = userService.deleteUser(id);
+        if(deleted) return ResponseEntity.noContent().build();
+        else return ResponseEntity.notFound().build();
     }
 }
